bfe100823f3909a416bef4d5ad24466a17a46a5d
ValGillet <val.gillet12@gmail.com>
2026-01-20 17:43:54 +0100

    improved handling of info files

--- io/volumescope.py
+++ io/volumescope.py
@@ -1,8 +1,9 @@
-''' 
+'''
 Utilities for finding and reading metadata produced by the ThermoFisher Volumescope microscope.
 '''
 
 
+import logging
 import os
 import re
 
@@ -20,22 +21,41 @@ def get_tileset_resolution(tileset_path):
             if entry.name.endswith('.info'):
                 info = entry.path
                 break
-    
+
     if info is None:
         return None
-    
+
     with open(info, 'r') as f:
         content = f.readlines()
 
-    resolution = tuple(map(int, re.findall(r'\d+', content[5])))
+    resolution = None
+    for line in content:
+        if 'resolution' in line.lower() or 'pixel size' in line.lower():
+            matches = re.findall(r'\d+', line)
+            if len(matches) >= 2:
+                resolution = tuple(map(int, matches[:2]))
+                break
+
+    # Fallback to line 5 for backward compatibility with existing .info files
+    if resolution is None:
+        if len(content) > 5:
+            matches = re.findall(r'\d+', content[5])
+            if len(matches) >= 2:
+                resolution = tuple(map(int, matches[:2]))
+            else:
+                logging.warning(f'Could not determine resolution from .info file in {tileset_path}')
+                return None
+        else:
+            logging.warning(f'Could not determine resolution from .info file in {tileset_path} (insufficient lines)')
+            return None
 
     return (tileset_path, resolution)
 
 
 def get_tilesets(main_dir, resolution, dir_pattern, num_workers):
-    
-    # Get all directories containing tilesets that are present in main_dir    
-    tileset_dirs = glob(main_dir + '/*/')
+
+    # Get all directories containing tilesets that are present in main_dir
+    tileset_dirs = glob(os.path.join(main_dir, '*', ''))
 
     stack_list = []
     # Find the ones with the right resolution
